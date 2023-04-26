ca9c6837e5b5def358553b6d1a7b1db519a5847f
kyleteeter <kyleteeter12@gmail.com>
2023-04-25 23:08:00 -0500

    Url loader

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
-const withImages = require('next-images');
+const path = require('path');
+const nextImages = require('next-images');
+const urlLoader = require('url-loader');
 
 module.exports = withImages({
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192, // Convert images < 8kb to base64 strings
+            fallback: 'file-loader',
+            publicPath: `/_next/static/images/`, // Path to image folder
+            outputPath: `${options.isServer ? '../' : ''}static/images/`, // Path to image folder
+            name: '[name].[hash].[ext]', // File name pattern
+          },
+        },
+      ],
+    });
+
     return config;
   },
   eslint: {
