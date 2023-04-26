56073f14d659ca1bc63709c75ab57187baf6ae11
kyleteeter <kyleteeter12@gmail.com>
2023-04-25 23:23:54 -0500

    move image loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const nextImages = require('next-images');
 
+const imageLoader = ({ src, width, quality }) => {
+  return `https://media.graphassets.com/${src}?w=${width}&q=${quality || 75}`
+}
+
 module.exports = nextImages({
   eslint: {
     ignoreDuringBuilds: true,
@@ -7,9 +11,7 @@ module.exports = nextImages({
   images: {
     domains: ['media.graphassets.com'],
     loader: 'custom',
+    path: 'https://media.graphassets.com/',
   },
+  imageLoader
 });
-
-export default function imageLoader({ src, width, quality }) {
-  return `https://media.graphassets.com/${src}?w=${width}&q=${quality || 75}`
-}
