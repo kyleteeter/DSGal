064bb5b63007b9bb976cd94d10232739ee670c45
kyleteeter <kyleteeter12@gmail.com>
2023-06-11 21:08:37 -0500

    Update SEO config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,10 +1,10 @@
-const description = `Learn how to build modern marketing websites, with localization and SEO, using Hygraph, NextJS, Chakra UI, and Vercel.`
-const title = `Build Modern Marketing Websites with a Headless CMS`
-const url = `https://marketing-websites.withheadlesscms.com`
+const description = `Welcome to my portfolio website, Data Science Gal. Explore my projects, expertise, and achievements in the field of data science. Discover how I leverage data to uncover valuable insights, build predictive models, and drive informed decision-making. Get a glimpse of my diverse skill set, including machine learning, statistical analysis, data visualization, and more. Join me on this data-driven journey and immerse yourself in the exciting world of data science through my projects and blog posts.`
+const title = `Jiannina Pinto - Data Science Gal`
+const url = `https://datasciencegal.com`
 
 const seo = {
   title,
-  titleTemplate: '%s | Hygraph',
+  titleTemplate: '%s | DataScienceGal',
   description,
   openGraph: {
     description,
@@ -13,8 +13,8 @@ const seo = {
     url
   },
   twitter: {
-    handle: '@Hygraphcom',
-    site: '@Hygraphcom'
+    handle: '@DSGal',
+    site: '@DSGal'
   }
 }
 
