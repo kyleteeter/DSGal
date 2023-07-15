c5d429041f1765c7769fcd682eb43b412ac8563e
kyleteeter <kyleteeter12@gmail.com>
2023-07-14 23:17:38 -0500

    min Width

--- components/blocks/testimonial.js
+++ components/blocks/testimonial.js
@@ -34,6 +34,7 @@ export default function Testimonial({ content, person }) {
               src={person.company.logo.url}
               alt={person.company.logo.title}
               layout="responsive"
+              minWidth="0"
               loader={imageLoader}
             />
           </Box>
