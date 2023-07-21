7a93322c4bd6801c5d5e5139f15ec48f298215b5
kyleteeter <kyleteeter12@gmail.com>
2023-07-20 23:49:33 -0500

    error fix

--- pages/contact.js
+++ pages/contact.js
@@ -45,7 +45,7 @@ export default function contact({ navigation }) {
           <WrapItem w="30%">
             <Box m={16}>
               <Heading >Contact</Heading>
-              <Text mt={{ sm: 3, md: 3, lg: 5 }} color={subColor}">
+              <Text mt={{ sm: 3, md: 3, lg: 5 }} color={subColor}>
                 Send me an email or schedule a call if you would like to chat. 
               </Text>
               <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
