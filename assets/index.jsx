394df94f086a523e67c312b87aeb7ed0b6727560
kyleteeter <kyle.teeter@leadpages.com>
2025-03-20 15:17:25 -0500

    Update image

--- assets/index.jsx
+++ assets/index.jsx
@@ -1,3 +1,3 @@
 export { default as logowhite } from "./DSG-logo-white.png";
 export { default as logoblack } from "./DSG-logo-black.png";
-export { default as jiannina } from "./jiannina.jpeg";
+export { default as jiannina } from "./jiannina.jpg";
